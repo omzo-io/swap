7ccffedcf237104ff96f2d23e464e6ef39002a44
Stanislav <34024018+skvortsas@users.noreply.github.com>
2025-09-30 19:18:33 +0300

    merget stashed

--- apps/web/src/global/providers/Web3Provider.tsx
+++ apps/web/src/global/providers/Web3Provider.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { LastUsedTokensProvider } from '@/features';
 import { config, queryClient } from '@/global';
-import { midlRegtest } from '@midl-xyz/midl-js-executor';
 import { WagmiMidlProvider } from '@midl-xyz/midl-js-executor-react';
 import { MidlProvider } from '@midl-xyz/midl-js-react';
+import { midlRegtest } from '@midl-xyz/midl-js-executor';
 import { SatoshiKitProvider } from '@midl-xyz/satoshi-kit';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { http } from 'viem';
