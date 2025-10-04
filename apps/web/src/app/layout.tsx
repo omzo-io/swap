19051aae41ceedb96f38820b3011ce391e44bbc5
Stanislav <34024018+skvortsas@users.noreply.github.com>
2025-10-04 11:37:48 +0300

    refactor: favicon + metadata

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,12 +2,36 @@ import { Poppins } from "next/font/google";
 import type { ReactNode } from "react";
 import { css, cx } from "~/styled-system/css";
 import { ErrorScreen } from '@/widgets/error-screen';
+import { Metadata } from "next";
 import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "500", "600", "700"],
 });
+
+export const metadata: Metadata = {
+  title: 'Omzo Swap',
+  description: 'Web3 app for swapping Runes on Bitcoin',
+  metadataBase: new URL('https://omzo.io'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'de-DE': '/de-DE',
+    },
+  },
+  openGraph: {
+    title: 'Omzo Swap',
+    description: 'Web3 app for swapping Runes on Bitcoin',
+    url: 'https://omzo.io',
+    type: 'website',
+  },
+  twitter: {
+    title: 'Omzo Swap',
+    description: 'Web3 app for swapping Runes on Bitcoin',
+  },
+};
 
 
 const envMaintenance = (process.env.NEXT_PUBLIC_IS_MAINTENANCE ?? '') as string;
